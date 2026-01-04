44dacc30b7ebd53ea6a0bec8af8cf8c9a489c7ba
akshay-kakade <akshaykakade648@gmail.com>
2026-01-04 13:22:20 +0530

    deploy

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,14 @@
-from app import create_app
+import sys
+import os
+import traceback
 
-app = create_app()
+try:
+    from app import create_app
+    app = create_app()
+except Exception as e:
+    print("!!! ERROR DURING APP STARTUP !!!")
+    traceback.print_exc()
+    sys.exit(1)
 
 if __name__ == '__main__':
     app.run(debug=True)
