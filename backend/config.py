52ea0122e89c2c012245f33bc09feb777680aa86
akshay-kakade <akshaykakade648@gmail.com>
2026-01-05 19:12:20 +0530

    access token time extended

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,6 +7,8 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt-super-secret-key'
+    # Access tokens will now be valid for 5 hours
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=5)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///site.db'
